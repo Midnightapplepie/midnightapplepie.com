cfe64541a82ea64eed3b51196e1797580098feef
midnightapplepie <midnightapplepie@gmail.com>
2016-10-31 14:38:43 -0700

    updated css tag on server.js

--- app/server.js
+++ app/server.js
@@ -20,7 +20,7 @@ console.log(cssUrl);
 console.log(builtUrl);
 
 function layout(html, preloadedState){
-  var mainCss =  `<link rel="stylesheet" href="from server-${cssUrl}">`;
+  var mainCss =  `<link rel="stylesheet" href=${cssUrl}>`;
   var bootStrap = `<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">`;
   var reactScript = `<script src=${builtUrl}></script>`
   
